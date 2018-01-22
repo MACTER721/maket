3c8b7923d835a31913f1eb240f0b0436b1c62acf
Olesya_Gataulina <olg@crtweb.ru>
2018-01-22 10:43:17 +0500

    init

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,2 +1,8 @@
-import '../../components/slider'
-import '../../components/map/map.js'
+import '../../components/form'
+import '../../components/form/adress.js'
+import '../../components/form/input.js'
+// import '../../components/form/document.js'
+import '../../components/form/new-document.js'
+import '../../components/form/radio.js'
+import '../../components/form/submit.js'
+
